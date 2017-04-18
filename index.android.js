753e567c04a5726b22a27e50a865830f9c59eff3
menglb <lingbinmeng@hotmail.com>
2017-04-18 12:24:16 +0800

    添加android环境下的mqtt测试。

--- index.android.js
+++ index.android.js
@@ -4,50 +4,62 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
 } from 'react-native';
+import MqttClient from './app/mqtt'
+import MqttWebsocket from './app/mqtt.websocket'
 
 export default class MqttTest extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    Hello World!
+                </Text>
+                <Text style={styles.instructions}>
+                    123456
+                </Text>
+                <MqttClient
+                    style={{
+                        height: 200,
+                        flex:1,
+                        alignSelf:  'stretch' ,
+                      }}
+                />
+                <MqttWebsocket
+                    style={{
+                        height: 200,
+                        flex:1,
+                        alignSelf:  'stretch' ,
+                      }}
+                />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
 
 AppRegistry.registerComponent('MqttTest', () => MqttTest);
